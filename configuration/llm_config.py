80396f0c9df7cd257fb6e2a33bdb75d111d8eabd
Aryan <aryanagrawal252007@gmail.com>
2026-01-29 21:10:23 +0530

    Fix :added chroma_db to git ignore

--- configuration/llm_config.py
+++ configuration/llm_config.py
@@ -16,7 +16,7 @@ class OpenAILLMConfig:
 
 llm_config_map = {
     "gpt-4o": OpenAILLMConfig(
-        model="vidia/nemotron-nano-9b-v2:free",
+        model="nvidia/nemotron-nano-9b-v2:free",
         api_key=os.environ.get("OPENROUTER_API_KEY"),
         base_url=os.environ.get("OPENAI_API_BASE"),
         
